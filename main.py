71862f2b471e5a6ba606af19a8324784779e67ab
AccountZhou <81340730+AccountZhou@users.noreply.github.com>
2024-05-16 13:19:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_detection_folder():
 
 if __name__ == '__main__':
 
-    st.title('YOLOv5 Streamlit App')
+    st.title('YOLOv5 Streamlit测试')
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str,
